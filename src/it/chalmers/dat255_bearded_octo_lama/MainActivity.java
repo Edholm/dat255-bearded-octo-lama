94be2d44166c9fc09bc1fe96d2f9f0b58e678e44
Johan Gustafsson <johan.gusten@gmail.com>
2012-09-18 23:07:59 +0200

    Made MainActivity into a ListActivity and hooked up a simple ListView to test our layout. Also cleaned the layout for the activity since it's for now not being used. May use it later so won't delete it just yet

--- src/it/chalmers/dat255_bearded_octo_lama/MainActivity.java
+++ src/it/chalmers/dat255_bearded_octo_lama/MainActivity.java
@@ -1,15 +1,30 @@
 package it.chalmers.dat255_bearded_octo_lama;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends ListActivity {
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        String[] testValues = new String [] {"Activate alarm", "Set alarm"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, testValues);
+        
+        
+        setListAdapter(adapter);
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+      String item = (String) getListAdapter().getItem(position);
+      Toast.makeText(this, item + " clicked", Toast.LENGTH_LONG).show();
     }
 
     @Override
