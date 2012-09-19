b6d6aa42b4a76cfe26ed76a025dec9270a3ed018
Johan Gustafsson <johan.gusten@gmail.com>
2012-09-19 17:30:38 +0200

    Added some simple buttons and a analog clock to mainActivity layout to serve as example code. Also set both Notification and Settings activity to exported=false to stop eclipse from showing a warning about permissions

--- src/it/chalmers/dat255_bearded_octo_lama/MainActivity.java
+++ src/it/chalmers/dat255_bearded_octo_lama/MainActivity.java
@@ -1,7 +1,13 @@
 package it.chalmers.dat255_bearded_octo_lama;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
-	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
 }
