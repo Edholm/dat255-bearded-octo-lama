0b85e160b4fb28bb63e938fb2c1dd5beac1e08a5
Johan Gustafsson <johan.gusten@gmail.com>
2012-09-26 10:46:16 +0200

    Changed the anchors of some textviews in layout for MainActivity. Also added so the textView for the currentTime and currentDate automatically updates when onResume() in MainActivity runs

--- src/it/chalmers/dat255_bearded_octo_lama/MainActivity.java
+++ src/it/chalmers/dat255_bearded_octo_lama/MainActivity.java
@@ -1,13 +1,35 @@
 package it.chalmers.dat255_bearded_octo_lama;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	TextView currentTimeView, currentDateView;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
         
+        currentTimeView = (TextView) findViewById(R.id.currentTime);
+        currentDateView = (TextView) findViewById(R.id.currentDate);
     }
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		String currentTimeString = new SimpleDateFormat("HH:mm").format(new Date());
+		String currentDateString = DateFormat.getDateInstance().format(new Date());
+		
+		currentTimeView.setText(currentTimeString);
+		currentDateView.setText(currentDateString);
+	}
+	
 }
