4007ac2c00af75cc074872e9a1e88bb9a932dc39
Johan Gustafsson <johan.gusten@gmail.com>
2012-09-26 11:13:09 +0200

    Cleaned up unused imports

--- src/it/chalmers/dat255_bearded_octo_lama/MainActivity.java
+++ src/it/chalmers/dat255_bearded_octo_lama/MainActivity.java
@@ -7,7 +7,6 @@ import java.util.Date;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
