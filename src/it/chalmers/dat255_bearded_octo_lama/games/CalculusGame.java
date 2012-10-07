977ee28e42629d454c14c240107eec9d57fb96cd
Johan Gustafsson <johan.gusten@gmail.com>
2012-10-07 19:25:15 +0200

    Added missing copyrights and license to a few classes

--- src/it/chalmers/dat255_bearded_octo_lama/games/CalculusGame.java
+++ src/it/chalmers/dat255_bearded_octo_lama/games/CalculusGame.java
@@ -1,4 +1,23 @@
 package it.chalmers.dat255_bearded_octo_lama.games;
+/**
+ * Copyright (C) 2012 Emil Edholm, Emil Johansson, Johan Andersson, Johan Gustafsson
+ *
+ * This file is part of dat255-bearded-octo-lama
+ *
+ *  dat255-bearded-octo-lama is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  dat255-bearded-octo-lama is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with dat255-bearded-octo-lama.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 import it.chalmers.dat255_bearded_octo_lama.R;
 
