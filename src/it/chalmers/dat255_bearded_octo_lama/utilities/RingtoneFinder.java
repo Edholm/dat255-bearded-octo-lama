ea91234303ed22065e0dd3d88803576708d4ef3f
Johan Gustafsson <johan.gusten@gmail.com>
2012-10-15 00:01:55 +0200

    Added copyright and licenses to xml files and to RingtoneFInder

--- src/it/chalmers/dat255_bearded_octo_lama/utilities/RingtoneFinder.java
+++ src/it/chalmers/dat255_bearded_octo_lama/utilities/RingtoneFinder.java
@@ -1,7 +1,25 @@
+/**
+ * Copyright (C) 2012 Emil Edholm, Emil Johansson, Johan Andersson, Johan Gustafsson
+ *
+ * This file is part of dat255-bearded-octo-lama
+ *
+ *  dat255-bearded-octo-lama is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  dat255-bearded-octo-lama is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with dat255-bearded-octo-lama.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package it.chalmers.dat255_bearded_octo_lama.utilities;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import android.app.Activity;
